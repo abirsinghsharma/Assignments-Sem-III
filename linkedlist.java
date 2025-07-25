16f991b17842adc943123e7d612c6268dadc4487
Abir Singh Sharma <abirsinghsharma@gmail.com>
2025-07-25 12:50:41 +0530

    linkedlist.java

---
+++ linkedlist.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import java.util.LinkedList;
+public class Main{
+    Node head;
+    static class Node{
+        int data;
+        Node next;
+        Node(int d){
+          data = d;
+          next = null;
+        }
+    }
+    public void addData(int data){
+        Node newdata = new Node(data);
+        if (head == null){
+            head = newdata;
+        }else{
+            Node last = head;
+            while (last.next != null){
+               last = last.next;
+            }
+            last.next = newdata;
+        }
+    }
+    public void printData(){
+        Node currentNode = head;
+        System.out.println("Linked List:");
+        while (currentNode != null) { 
+            System.out.print(currentNode.data + " ");
+            currentNode = currentNode.next;
+        }
+        System.out.println();
+    }
+    
+	public static void main(String[] args) {
+       LinkedList list = new LinkedList();
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("Enter the number of elements to store:");
+       int n = scanner.nextInt();
+       for (int i = 0; i < n; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            int data = scanner.nextInt(); 
+            list.addData(); 
+        }
+        list.printData(); 
+        scanner.close(); 
+    }
+} 
