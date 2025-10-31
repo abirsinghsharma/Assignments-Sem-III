9c65eaee0a8d542fd1cbb3366cae4b0ece78eaf5
Abir Singh Sharma <abirsinghsharma@gmail.com>
2025-10-31 12:32:37 +0530

    Bubble Sort

---
+++ BubbleSort.java
@@ -0,0 +1,23 @@
+package test;
+
+public class BubbleSort {
+public static void bubblesort(int[] arr) {
+int n=arr.length;
+for(int i=0;i&lt;n;i++) {
+for(int j=0;j&lt;n-i-1;j++) {
+if(arr[j]&gt;arr[j+1]) {
+int temp=arr[j];
+arr[j]=arr[j+1];
+arr[j+1]=temp;
+}
+}
+}
+}
+public static void main(String[] args) {
+int[] arr = {1,3,5,7,9,2,4,6,8,0};
+bubblesort(arr);
+for (int num : arr) {
+System.out.print(&quot; &quot;+num);
+}
+}
+}
